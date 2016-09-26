c3bca3462fc5fda50732df228aacf8c249a70b53
dicristomanuel <manuel.dicristo@raise.com>
2016-09-25 21:29:39 -0500

    Initial Commit

---
+++ firstIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstIOS
+//
+//  Created by Manuel Di Cristo on 9/25/16.
+//  Copyright © 2016 manueldicristo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ firstIOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  firstIOS
+//
+//  Created by Manuel Di Cristo on 9/25/16.
+//  Copyright © 2016 manueldicristo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
